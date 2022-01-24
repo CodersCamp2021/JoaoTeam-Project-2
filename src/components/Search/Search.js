57f989bb2089987c06bbd15a1829b7e8502142d3
Justyna <justyna.chmielinska@coderscrew.pl>
2022-01-24 18:24:40 +0100

    styling

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -6,25 +6,27 @@ export default function SearchWindow() {
     const [language, updateLanguage] = useState("");
 
     return (
-        <div className="search-params">
+        <div className="search-window">
             <form>
                 <label htmlFor="location">
-                    Best coders who live...
+                    <h5 className="inline-text">Best coders who live...</h5>
                     <input
                         id="location"
                         value={location}
                         placeholder="anywhere in Poland"
-                        onChange={(e) => updateLocation(e.target.value)} />
+                        onChange={(e) => updateLocation(e.target.value)}
+                        className="input-area" />
                 </label>
                 <label htmlFor="language">
-                    and writes in...
+                    <h5 className="inline-text">and writes in...</h5>
                     <input
                         id="language"
                         value={language}
                         placeholder="any language"
-                        onChange={(e) => updateLanguage(e.target.value)} />
+                        onChange={(e) => updateLanguage(e.target.value)}
+                        className="input-area" />
                 </label>
-                <input type="submit" value="Find coders" />
+                <input type="submit" value="Find coders" className="submitBtn" />
             </form>
         </div>
     )
