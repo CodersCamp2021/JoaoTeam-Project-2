0422ffaa97811d046adf238e8b101ae9f290408d
Justyna <justyna.chmielinska@coderscrew.pl>
2022-01-26 00:06:40 +0100

    main page layout

---
+++ src/components/MainPage/MainPage.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const MainPage = () => {
+    return (
+        <div className="main-page-container">
+            <img className="logo" />
+            <h3 className="intro">Hey Buddy! who are you <br />
+                <span className="looking">Looking</span> for?</h3>
+
+        </div>
+    )
+}
+
+export default MainPage
