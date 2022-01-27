820cc14c9120c8b0e4e3bf279c4695d12da7a876
spidero7 <pajak.bartlomiej97@gmail.com>
2022-01-27 21:42:00 +0100

    Started switch button finctionality

--- src/components/Results.js
+++ src/components/Results.js
@@ -6,11 +6,12 @@ const Results = () => {
 	// Draft
 	const city = "Kraków";
 	const language = "Javascript";
+	const sort = "best match";
 
 	useEffect(() => {
 		const fetchData = async () => {
 			const data = await fetch(
-				`https://api.github.com/search/users?q=location:${city}&q=language:${language}&per_page=20`
+				`https://api.github.com/search/users?q=location:${city} language:${language}&per_page=20&sort=${sort}`
 			);
 			const json = await data.json();
 			const items = json.items;
@@ -29,6 +30,16 @@ const Results = () => {
 					<img className="details" />
 					<div className="details-city-language">
 						{city}, {language}
+					</div>
+				</div>
+				<div>
+					<div>
+						order by: score{" "}
+						<span
+							onClick={(e) => console.log("click")}
+							className="switch-btn"
+						></span>{" "}
+						followers
 					</div>
 				</div>
 				<div className="users-container">
