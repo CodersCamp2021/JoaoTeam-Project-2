fbccd5a1708421f6d3c737653f7843de0edba49b
itspatys <patryk.pierzchaa@gmail.com>
2022-02-01 12:26:10 +0100

    Created basic structure.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { render } from "react-dom"
 
 import TestComponent from "./components/TestComponent/TestComponent"
+import User from "./components/User/User"
 
 const App = () => {
     return (
-        <TestComponent />
+        // <TestComponent />
+        <User />
     )
 }
 
---
+++ src/components/User/User.js
@@ -0,0 +1,18 @@
+const User = () => {
+    return (
+        <div className="user">
+            <div className="user__img"></div>
+            <div className="user__content">
+                <div className="user__name"></div>
+                <div className="user__fullname"></div>
+                <div className="user__desc"></div>
+                <div className="user__website"></div>
+                <div className="user__mail"></div>
+                <div className="user__job"></div>
+            </div>
+            <div className="user__link"></div>
+        </div>
+    )
+}
+
+export default User
