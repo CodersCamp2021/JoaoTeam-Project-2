51dda695cd77fedb525b30b3fb6b3ef133f19561
Justyna <justyna.chmielinska@coderscrew.pl>
2022-02-02 13:31:58 +0100

    router begining

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { render } from "react-dom"
-import TestComponent from "./components/TestComponent/TestComponent"
-//import MainPage from "./components/MainPage/MainPage"
+// import TestComponent from "./components/TestComponent/TestComponent"
+import MainPage from "./components/MainPage/MainPage"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 const App = () => {
     return (
-        <div>
-            <TestComponent />
-        </div>
+        <Router>
+            <div>
+                <Route path="/" component={MainPage} />
+            </div>
+        </Router>
     )
 }
 
