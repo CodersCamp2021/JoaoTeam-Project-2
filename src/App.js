8764cdd83f486cbb5ea17b7bc088e8ee7c7e38ab
werpietrzak <weropietrzak@gmail.com>
2022-02-05 16:01:14 +0100

    created ranking component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import { render } from "react-dom"
+import { render } from "react-dom";
 // import TestComponent from "./components/TestComponent/TestComponent"
-import MainPage from "./components/MainPage/MainPage"
-import Results from "./components/ResultsPage/Results"
-import PageNotFound from "./components/Errors/PageNotFound"
-import User from "./components/User/User"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import MainPage from "./components/MainPage/MainPage";
+import Results from "./components/ResultsPage/Results";
+import PageNotFound from "./components/Errors/PageNotFound";
+import User from "./components/User/User";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Ranking from "./components/Ranking";
 
 const App = () => {
-    return (
-        <Router>
+	return (
+		<Ranking />
+		/*<Router>
             <Routes>
                 <Route path="/" element={< MainPage />} />
                 <Route path="/results" element={<Results />} />
                 <Route path="/user/:login" element={<User />} />
                 <Route path="*" element={<PageNotFound />} />
             </Routes>
-        </Router >
-    )
-}
+        </Router >*/
+	);
+};
 
 render(<App />, document.getElementById("root"));
---
+++ src/components/Ranking.js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import Toggle from "./ResultsPage/Toggle.js"
+
+function Ranking() {
+	const [users, setUsers] = useState([]);
+	const [toggled, setToggled] = useState(false);
+	const [order, setOrder] = useState("repositories");
+
+	useEffect(() => {
+		async function requestData() {
+			const res = await fetch(
+				`https://api.github.com/search/users?q=location:$poland&per_page=20&sort=${order}`
+			);
+			const json = await res.json();
+			const items = json.items;
+
+			setUsers(items);
+		}
+		requestData().catch(console.error);
+	}, []);
+
+	const handleOrder = () => {
+		if (order === "repositories") {
+			setOrder("followers");
+		} else if (order === "followers") {
+			setOrder("repositories");
+		}
+	};
+
+	return (
+		<div>
+			<div className="results-container">
+				<img className="logo" />
+				<div className="details-container">
+					<div className="details"></div>
+					<div className="details-city-language">
+						Top 20 in Poland - Followers
+					</div>
+				</div>
+				<div>
+					<div className="switch-container">
+						<div>Order by: Repositories</div>
+						<Toggle
+							onChange={(event) => setToggled(event.target.checked)}
+							onChange={handleOrder}
+						/>
+						<div>Followers</div>
+						{console.log(order)}
+					</div>
+				</div>
+				<div className="users-container">
+					{users.map((user) => {
+						return (
+							<div className="user-container">
+								<img
+									className="user-img"
+									src={user.avatar_url}
+									alt={user.login}
+									key={user.login}
+								/>
+								<h2>{user.login}</h2>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Ranking;
