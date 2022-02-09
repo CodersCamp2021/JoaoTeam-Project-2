ca2298523cbc9b9d0f7de6ee43241c2199f502e5
Justyna <justyna.chmielinska@coderscrew.pl>
2022-02-09 11:46:32 +0100

    adding link to logo on main page

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,10 +1,11 @@
 import React from "react"
 import SearchWindow from "../Search/Search"
+import { Link } from "react-router-dom"
 
 const MainPage = () => {
     return (
         <div className="main-page-container">
-            <Link to="/">
+            <Link to="/" className="logo-link">
                 <img className="logo" />
             </Link>
             <h3 className="intro">Hey Buddy! Who are you <br />
